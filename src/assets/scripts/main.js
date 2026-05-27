185e53740d27d46dd0a394d85eacc3ba93ae8144
malegrera <malegrera@uoc.edu>
2026-05-27 09:40:04 +0200

    inicial

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,10 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
+
 /**
  * Write any other JavaScript below
  */
 
-+(function () {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-})();
+
+
